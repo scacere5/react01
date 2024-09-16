fe390d7d4271b981e53d93b9ff8937664a8b214c
scaceres5 <scaceres5@cuc.edu.co>
2024-09-16 09:42:59 -0500

    react01

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+import miLogo from "./assets/img/logoReact.png"
+
+function App() {
+
+  return (
+    <>
+     <img src={miLogo} alt="Logo React" />
+     <h1>¡¡ Hola!!</h1>
+     <p>Este es mi primer proyecto en React...</p>
+    </>
+  )
+}
+
+export default App
